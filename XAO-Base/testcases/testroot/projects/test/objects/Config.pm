00a6714ebad16c34ddb15a95fff186c9c8ac5a87
am <am>
2001-11-10 00:30:59 +0000

    Lost file

---
+++ XAO-Base/testcases/testroot/projects/test/objects/Config.pm
@@ -0,0 +1,14 @@
+package XAO::DO::Config;
+use strict;
+use XAO::Objects;
+use base XAO::Objects->load(objname => 'Config', baseobj => 1);
+
+sub init ($$) {
+    my $self=shift;
+
+    my $lc=XAO::Objects->new(objname => 'LocalConf', sitename => 'test');
+
+    $self->embed('local' => $lc)
+}
+
+1;
