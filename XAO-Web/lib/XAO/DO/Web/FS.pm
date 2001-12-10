38e724dddd939a37f7177ec6441ec8d7e19f3fe5
am <am>
2001-12-10 02:45:49 +0000

    FS added from xaotech site

---
+++ XAO-Web/lib/XAO/DO/Web/FS.pm
@@ -0,0 +1,90 @@
+=head1 NAME
+
+XAO::DO::Web::FS - XAO::Web front end object for XAO::FS
+
+=head1 SYNOPSIS
+
+<%FS uri="/Categories/123/description" path="/bits/cat-text"%>
+
+=head1 DESCRIPTION
+
+Web::FS allows web site developer to directly access XAO Foundation
+Server from templates without implementing specific objects.
+
+=cut
+
+###############################################################################
+package XAO::DO::Web::FS;
+use strict;
+use XAO::Utils;
+use base XAO::Objects->load(objname => 'Web::Action');
+
+use vars qw($VERSION);
+($VERSION)=(q$Id: FS.pm,v 1.1 2001/12/10 02:45:49 am Exp $ =~ /(\d+\.\d+)/);
+
+sub check_mode ($%) {
+    my $self=shift;
+    my $args=get_args(\@_);
+    my $mode=$args->{mode};
+
+    if($mode eq 'show-property') {
+        $self->show_property($args);
+    }
+    else {
+        $self->throw("check_mode - unknown mode '$mode'");
+    }
+}
+
+sub show_property ($%) {
+    my $self=shift;
+    my $args=get_args(\@_);
+
+    my $uri=$args->{uri} ||
+        $self->throw('property - no Foundation Server URI');
+
+    my $value;
+
+    my $cache;
+    if($args->{cache}) {
+        $cache=$self->siteconfig->get('fs_cache');
+        if($cache) {
+            if(exists $cache->{$uri}) {
+                $value=$cache->{$uri};
+            }
+        } else {
+            $self->siteconfig->put('fs_cache' => { });
+        }
+    }
+
+    $value=$self->odb->fetch($uri) unless defined $value;
+    $value=$args->{default} unless defined $value;
+    $value='' unless defined $value;
+
+    $cache->{$uri}=$value if $cache;
+
+    $self->textout($value);
+}
+
+###############################################################################
+1;
+__END__
+
+=head1 METHODS
+
+No publicly available methods except overriden display().
+
+=head1 EXPORTS
+
+Nothing.
+
+=head1 AUTHOR
+
+Copyright (c) 2000-2001 XAO, Inc.
+
+Andrew Maltsev <am@xao.com>.
+
+=head1 SEE ALSO
+
+Recommended reading:
+L<XAO::Web>,
+L<XAO::DO::Web::Page>.
