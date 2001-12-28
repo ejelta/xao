8b570f137540425a9ed4648b5c39805232af5528
am <am>
2001-12-28 02:52:32 +0000

    Initial import of older PodView

---
+++ XAO-PodView/PodView.pm
@@ -0,0 +1,294 @@
+=head1 NAME
+
+XAO::DO::Web::PodView - POD files viewer for XAO::Web
+
+=head1 SYNOPSIS
+
+ <%PodView module="XAO::Web"%>
+
+=head1 DESCRIPTION
+
+This displayable object for XAO::Web parses pod documentation inside
+installed perl modules and displays it. How it will look depends on a
+set of templates you provide and can theoretically vary from HTML to
+PostScript to XML as no specific HTML code is included in the module
+itself.
+
+You can use this object to view documentation for all Perl modules
+installed in your system.
+
+Default templates allow you to put the followin into your templates to
+display this manual pages for example:
+
+ <%PodView module="XAO::DO::Web::PodView"%>
+
+Alternatively you can give it a path to the file that would contain
+pod documentation after being processed by XAO::Page. This way you can
+even create and display dynamic POD documents.
+
+ <%PodView path="/bits/example.pod"%>
+
+By default it assumes that the output is "html" and uses appropriate
+=begin, =for and =end blocks. If you want to change that feel free to
+pass "format" argument:
+
+ <%PodView module="IO::File" format="ps"%>
+
+That will also change directory prefix for all templates internally used
+by PodView object -- you will need to provide them. And please, please,
+please -- if you're PostScript guru who can create a set of templates
+that will produce nice looking PostScript document -- do a favor for the
+Perl community and share it.
+
+The following templates are shipped with the PodView object and should
+be modified if you want. Remember, that you do not need to modify them
+in system directory - just copy them to your site and modify them there;
+you can even modify only those that you need to.
+
+ bits/podview/html/command-back         =back
+ bits/podview/html/command-head1        =head1
+ bits/podview/html/command-head2        =head2
+ bits/podview/html/command-item         =item
+ bits/podview/html/command-over         =over
+ bits/podview/html/command-unknown      =XXX
+ bits/podview/html/embed-bold           B<text>
+ bits/podview/html/embed-code           C<text>
+ bits/podview/html/embed-escape         E<text>
+ bits/podview/html/embed-file           F<text>
+ bits/podview/html/embed-italic         I<text>
+ bits/podview/html/embed-link-man       L<system manpage>
+ bits/podview/html/embed-link-pod       L<perl module>
+ bits/podview/html/embed-link-url       L<http://site.com/>
+ bits/podview/html/embed-nbsp           S<text>
+ bits/podview/html/embed-unknown        unknown<text>
+ bits/podview/html/pod-start            start of the document
+ bits/podview/html/pod-stop             end of the document
+ bits/podview/html/textblock-start      start of text paragraphs
+ bits/podview/html/textblock-stop       end of paragraphs
+ bits/podview/html/textblock-text       paragraph body
+ bits/podview/html/verbatim-start       start of verbatim paragraph
+ bits/podview/html/verbatim-stop        stop of verbatim paragraph
+ bits/podview/html/verbatim-text        verbatim paragraph body
+
+A little explanation is required for start and stop for textblock and
+verbatim. PodView calls start before a set of continous textblock or
+verbatim paragraphs.
+
+Normally start and stop for textblock are empty with <P> and </P>
+included in textblock-body. For verbatim mode start contains <PRE> and
+stop contains </PRE>. That means in effect that empty line between two
+verbatim paragraphs would not break verbatim mode as it happens with all
+pod processors I looked at.
+
+And at the same time if you do want to treat verbatim paragraphs one by
+one - you're free to do so.
+
+=head1 METHODS
+
+No publicly available methods except overriden display().
+
+=head1 EXPORTS
+
+Nothing.
+
+=head1 AUTHOR
+
+Andrew Maltsev <am@amsoft.ru>.
+
+=head1 SEE ALSO
+
+Recommended reading:
+L<XAO::Web>,
+L<XAO::DO::Web::Page>.
+
+=cut
+
+###############################################################################
+package XAO::DO::Web::PodView;
+use strict;
+use IO::File;
+use IO::String;
+use Pod::Parser;
+use XAO::Utils;
+
+use base XAO::Objects->load(objname => 'Web::Page');
+
+use vars qw($VERSION);
+($VERSION)=(q$Id: PodView.pm,v 1.1 2001/12/28 02:52:32 am Exp $ =~ /(\d+\.\d+)/);
+
+##
+# List of entities from Pod::Checker. I wonder who originally wrote that
+# code? It seems that everyone "borrows" it from some place else :)
+#
+my %ENTITIES = (
+ # Some normal chars that have special meaning in SGML context
+ amp    => '&',  # ampersand 
+'gt'    => '>',  # greater than
+'lt'    => '<',  # less than
+ quot   => '"',  # double quote
+
+ # PUBLIC ISO 8879-1986//ENTITIES Added Latin 1//EN//HTML
+ AElig	=> 'Æ',  # capital AE diphthong (ligature)
+ Aacute	=> 'Á',  # capital A, acute accent
+ Acirc	=> 'Â',  # capital A, circumflex accent
+ Agrave	=> 'À',  # capital A, grave accent
+ Aring	=> 'Å',  # capital A, ring
+ Atilde	=> 'Ã',  # capital A, tilde
+ Auml	=> 'Ä',  # capital A, dieresis or umlaut mark
+ Ccedil	=> 'Ç',  # capital C, cedilla
+ ETH	=> 'Ð',  # capital Eth, Icelandic
+ Eacute	=> 'É',  # capital E, acute accent
+ Ecirc	=> 'Ê',  # capital E, circumflex accent
+ Egrave	=> 'È',  # capital E, grave accent
+ Euml	=> 'Ë',  # capital E, dieresis or umlaut mark
+ Iacute	=> 'Í',  # capital I, acute accent
+ Icirc	=> 'Î',  # capital I, circumflex accent
+ Igrave	=> 'Ì',  # capital I, grave accent
+ Iuml	=> 'Ï',  # capital I, dieresis or umlaut mark
+ Ntilde	=> 'Ñ',  # capital N, tilde
+ Oacute	=> 'Ó',  # capital O, acute accent
+ Ocirc	=> 'Ô',  # capital O, circumflex accent
+ Ograve	=> 'Ò',  # capital O, grave accent
+ Oslash	=> 'Ø',  # capital O, slash
+ Otilde	=> 'Õ',  # capital O, tilde
+ Ouml	=> 'Ö',  # capital O, dieresis or umlaut mark
+ THORN	=> 'Þ',  # capital THORN, Icelandic
+ Uacute	=> 'Ú',  # capital U, acute accent
+ Ucirc	=> 'Û',  # capital U, circumflex accent
+ Ugrave	=> 'Ù',  # capital U, grave accent
+ Uuml	=> 'Ü',  # capital U, dieresis or umlaut mark
+ Yacute	=> 'Ý',  # capital Y, acute accent
+ aacute	=> 'á',  # small a, acute accent
+ acirc	=> 'â',  # small a, circumflex accent
+ aelig	=> 'æ',  # small ae diphthong (ligature)
+ agrave	=> 'à',  # small a, grave accent
+ aring	=> 'å',  # small a, ring
+ atilde	=> 'ã',  # small a, tilde
+ auml	=> 'ä',  # small a, dieresis or umlaut mark
+ ccedil	=> 'ç',  # small c, cedilla
+ eacute	=> 'é',  # small e, acute accent
+ ecirc	=> 'ê',  # small e, circumflex accent
+ egrave	=> 'è',  # small e, grave accent
+ eth	=> 'ð',  # small eth, Icelandic
+ euml	=> 'ë',  # small e, dieresis or umlaut mark
+ iacute	=> 'í',  # small i, acute accent
+ icirc	=> 'î',  # small i, circumflex accent
+ igrave	=> 'ì',  # small i, grave accent
+ iuml	=> 'ï',  # small i, dieresis or umlaut mark
+ ntilde	=> 'ñ',  # small n, tilde
+ oacute	=> 'ó',  # small o, acute accent
+ ocirc	=> 'ô',  # small o, circumflex accent
+ ograve	=> 'ò',  # small o, grave accent
+ oslash	=> 'ø',  # small o, slash
+ otilde	=> 'õ',  # small o, tilde
+ ouml	=> 'ö',  # small o, dieresis or umlaut mark
+ szlig	=> 'ß',  # small sharp s, German (sz ligature)
+ thorn	=> 'þ',  # small thorn, Icelandic
+ uacute	=> 'ú',  # small u, acute accent
+ ucirc	=> 'û',  # small u, circumflex accent
+ ugrave	=> 'ù',  # small u, grave accent
+ uuml	=> 'ü',  # small u, dieresis or umlaut mark
+ yacute	=> 'ý',  # small y, acute accent
+ yuml	=> 'ÿ',  # small y, dieresis or umlaut mark
+
+ # Some extra Latin 1 chars that are listed in the HTML3.2 draft (21-May-96)
+ copy   => '©',  # copyright sign
+ reg    => '®',  # registered sign
+ nbsp   => "\240", # non breaking space
+
+ # Additional ISO-8859/1 entities listed in rfc1866 (section 14)
+ iexcl  => '¡',
+ cent   => '¢',
+ pound  => '£',
+ curren => '¤',
+ yen    => '¥',
+ brvbar => '¦',
+ sect   => '§',
+ uml    => '¨',
+ ordf   => 'ª',
+ laquo  => '«',
+'not'   => '¬',    # not is a keyword in perl
+ shy    => '­',
+ macr   => '¯',
+ deg    => '°',
+ plusmn => '±',
+ sup1   => '¹',
+ sup2   => '²',
+ sup3   => '³',
+ acute  => '´',
+ micro  => 'µ',
+ para   => '¶',
+ middot => '·',
+ cedil  => '¸',
+ ordm   => 'º',
+ raquo  => '»',
+ frac14 => '¼',
+ frac12 => '½',
+ frac34 => '¾',
+ iquest => '¿',
+'times' => '×',    # times is a keyword in perl
+ divide => '÷',
+
+# some POD special entities
+ verbar => '|',
+ sol => '/'
+);
+
+##
+# Loading and displaying perl module documentation.
+#
+sub display ($%)
+{ my $self=shift;
+  my $args=get_args(\@_);
+
+  ##
+  # Creating parser
+  #
+  my $parser=XAO::Pod::Parser->new($self->object(),$args->{format} || 'html');
+
+  ##
+  # Input handler
+  #
+  my $ih;
+
+  ##
+  # Do we have path to pod document?
+  #
+  if($args->{path} || $args->{template})
+   { my $text=$self->object->expand($args);
+     $ih=IO::String->new($text);
+   }
+
+  ##
+  # Finding file for the given module
+  #
+  if(!$ih && $args->{module})
+   { my $file=$parser->find_module_file($args->{module});
+     if(!$file)
+      { $self->object->display(path => $args->{error}) if $args->{error};
+        return;
+      }
+     $ih=IO::File->new;
+     if(!$ih->open($file))
+      { $self->object->display(path => $args->{error}) if $args->{error};
+        return;
+      }
+   }
+
+  ##
+  # No input?? Too bad.
+  #
+  if(!$ih)
+   { $self->object->display(path => $args->{error}) if $args->{error};
+     return;
+   }
+
+  ##
+  # Parsing
+  #
+  my $oh=IO::String->new();
+  $parser->parse_from_filehandle($ih,$oh);
+}
+
+###############################################################################
+1;
---
+++ XAO-PodView/lib/XAO/DO/Web/PodView/Parser.pm
@@ -0,0 +1,367 @@
+=head1 NAME
+
+XAO::DO::Web::PodView::Parser - POD parser for XAO::DO::Web::PodView
+
+=head1 SYNOPSIS
+
+Should not be called directly, see XAO::DO::Web::PodView usage
+synopsis.
+
+=head1 DESCRIPTION
+
+Extends Pod::Parser class to allow Pod parsing for
+XAO::DO::Web::PodView.
+
+=head1 EXPORTS
+
+Nothing.
+
+=head1 AUTHOR
+
+Copyright (c) 2001 XAO Inc.
+
+Andrew Maltsev <am@xao.com>.
+
+=head1 SEE ALSO
+
+Recommended reading:
+L<XAO::Web>,
+L<XAO::DO::Web::PodView>.
+
+=cut
+
+###############################################################################
+package XAO::DO::Web::PodView::Parser;
+use XAO::Utils;
+use base qw(Pod::Parser);
+
+sub new {
+    my $self=shift->SUPER::new();
+    $self->{'-dispobj'}=$_[0];
+    die "No dispobj argument given" unless $self->{'-dispobj'};
+    $self->{'-format'}=lc($_[1] || 'html');
+    $self;
+}
+
+sub display {
+    my $self=shift;
+    $self->{'-dispobj'}->display(@_);
+}
+
+sub expand {
+    my $self=shift;
+    $self->{'-dispobj'}->expand(@_);
+}
+
+sub paragraph_text ($$) {
+    my $self=shift;
+    my $text=shift;
+    $text;
+}
+
+sub interpolate ($$$) {
+    my $self=shift;
+    my ($text,$line_num)=@_;
+    my $ptree=$self->parse_text( { -expand_seq => 'interior_sequence',
+                                   -expand_text => 'html_encode'
+                                 },
+                                 $text,$line_num);
+    return join('',$ptree->children());
+}
+
+sub html_encode ($$$)
+{ my $self=shift;
+  my $text=shift;
+  #dprint "html_encode: $text";
+  t2ht($text);
+}
+
+##
+# Returns path to the template file
+#
+sub path ($$)
+{ my $self=shift;
+  my $format=$self->{-format};
+  my $name=shift;
+  "/bits/podview/$format/$name";
+}
+
+sub command ($$$$)
+{ my $self=shift;
+  my ($command, $paragraph, $line_num) = @_;
+  $self->verbatim_stop($line_num);
+  $self->textblock_stop($line_num);
+  #dprint "command: command=$command paragraph=$paragraph line_num=$line_num";
+
+  ##
+  # Some special processing. Getting out of all levels of =over on
+  # =head1 and =head2; calculating level of =over.
+  #
+  # On =over we do not print anything and wait until we get first =item
+  # in that scope. The decide if it is enumerated list or not.
+  #
+  if($command eq 'over')
+   { $self->{-had_item}=0;
+     return;
+   }
+  elsif($command eq 'back')
+   { $self->{-over_level}--;
+   }
+  elsif($command eq 'item')
+   { if(! $self->{-had_item})
+      { $self->{-over_level}++;
+        $self->{-had_item}=1;
+        $self->display(path => $self->path("command-over"),
+                       TEXT => $self->paragraph_text($self->interpolate($paragraph,$line_num)),
+                       COMMAND => 'over',
+                       LINENUM => $line_num,
+                       UNPARSED => $paragraph,
+                       OVERLEVEL => $self->{-over_level}
+                      );
+      }
+   }
+  elsif($command eq 'head1' || $command eq 'head2')
+   { while($self->{-over_level})
+      { $self->command('back',$paragraph,$line_num);
+      }
+   }
+  elsif($command eq 'cut' || $command eq '=pod')
+   { return;
+   }
+  elsif($command eq 'for')
+   { my $format=$self->{-format};
+     return unless $paragraph =~ /^\s*($format)\s+(.*)$/;
+     $paragraph=$2;
+   }
+  elsif($command eq 'begin')
+   { return unless $paragraph => /^\s*(.*?)(\s+(.*))?$/;
+     push @{$self->{-begin_stack}},$1;
+   }
+  elsif($command eq 'end')
+   { return unless $paragraph => /^\s*(.*?)(\s+(.*))?$/;
+     my $f=$1;
+     if(!@{$self->{-begin_stack}})
+      { eprint ref($self)."::command - no '=begin' for '=end $f' at line $line_num";
+        return;
+      }
+     if($self->is_in_format($f))
+      { eprint ref($self)."::command - unmatched format '=end $f' at line $line_num";
+      }
+     pop @{$self->{-begin_stack}};
+     return;
+   }
+  else
+   { $self->display(path => $self->path("command-unknown"),
+                    TEXT => $self->paragraph_text($self->interpolate($paragraph,$line_num)),
+                    COMMAND => $command,
+                    LINENUM => $line_num,
+                    UNPARSED => $paragraph,
+                   );
+     return;
+   }
+
+  ##
+  # Displaying paragraph in the appropriate command template if we got
+  # here.
+  #
+  $self->display(path => $self->path("command-$command"),
+                 TEXT => $self->paragraph_text($self->interpolate($paragraph,$line_num)),
+                 COMMAND => $command,
+                 LINENUM => $line_num,
+                 UNPARSED => $paragraph
+                );
+}
+
+sub verbatim ($$$)
+{ my $self=shift;
+  my ($paragraph, $line_num) = @_;
+  return undef if !$self->{-verbatim_mode} && $paragraph =~ /^[\s\r\n]$/;
+  $self->textblock_stop($line_num);
+  #dprint "verbatim: paragraph=$paragraph line_num=$line_num";
+  if(! $self->{-verbatim_mode})
+   { $self->display(path => $self->path("verbatim-start"),
+                    TEXT => '',
+                    LINENUM => $line_num,
+                    UNPARSED => ''
+                   );
+     $self->{-verbatim_mode}=1;
+   }
+  $self->display(path => $self->path("verbatim-text"),
+                 TEXT => $self->paragraph_text($paragraph),
+                 LINENUM => $line_num,
+                 UNPARSED => $paragraph
+                );
+}
+
+sub verbatim_stop ($$)
+{ my $self=shift;
+  if($self->{-verbatim_mode})
+   { $self->display(path => $self->path('verbatim-stop'),
+                    TEXT => '',
+                    LINENUM => $_[0] || 0,
+                    UNPARSED => ''
+                   );
+     $self->{-verbatim_mode}=0;
+   }
+}
+
+sub textblock ($$$)
+{ my $self=shift;
+  my ($paragraph, $line_num) = @_;
+  return undef if $paragraph =~ /^[\s\r\n]$/;
+  $self->verbatim_stop($line_num);
+  #dprint "textblock: paragraph=|$paragraph| line_num=$line_num";
+  if(! $self->{-textblock_mode})
+   { $self->display(path => $self->path('textblock-start'),
+                    TEXT => '',
+                    LINENUM => $line_num,
+                    UNPARSED => ''
+                   );
+     $self->{-textblock_mode}=1;
+   }
+  $self->display(path => $self->path('textblock-text'),
+                 TEXT => $self->paragraph_text($self->interpolate($paragraph,$line_num)),
+                 LINENUM => $line_num,
+                 UNPARSED => $paragraph
+                );
+}
+
+sub textblock_stop ($$)
+{ my $self=shift;
+  if($self->{-textblock_mode})
+   { $self->display(path => $self->path('textblock-stop'),
+                    TEXT => '',
+                    LINENUM => $_[0] || 0,
+                    UNPARSED => ''
+                   );
+     $self->{-textblock_mode}=0;
+   }
+}
+
+##
+# Does not display what it gets, but returns it instead! Pay attention
+# to not add any code at the end as it uses "last value is returned"
+# style..
+#
+sub interior_sequence ($$$)
+{ my $self=shift;
+  my ($command,$text)=@_;
+  #dprint "iseq: command=$command, text=$text";
+  if($command eq 'I')
+   { $self->expand(path => $self->path('embed-italic'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'B')
+   { $self->expand(path => $self->path('embed-bold'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'S')
+   { $self->expand(path => $self->path('embed-nbsp'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'C')
+   { $self->expand(path => $self->path('embed-code'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'F')
+   { $self->expand(path => $self->path('embed-file'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'X')
+   { $self->expand(path => $self->path('embed-index'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'Z')
+   { $self->expand(path => $self->path('embed-zero'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+  elsif($command eq 'L')
+   { if($text =~ /^((.*)\|)?((http|ftp|news):\/\/.*)$/)
+      { my $url=$3;
+        my $comment=$2 || $url;
+        $self->expand(path => $self->path('embed-link-url'),
+                      COMMAND => $command,
+                      TEXT => $comment,
+                      URL => $url
+                     );
+      }
+     else
+      { $text =~ /^((.*?)\|)?(.*?)(\/(.*))?$/;
+        my $manpage=$3;
+        my $section=$5 || '';
+        my $comment=$2 || "the $manpage manpage";
+        return $comment unless $manpage;
+        if($self->find_module_file($manpage))
+         { $self->expand(path => $self->path('embed-link-pod'),
+                         COMMAND => $command,
+                         TEXT => $comment || '',
+                         MODULE => $manpage,
+                         SECTION => $section || ''
+                        );
+         }
+        else
+         { $self->expand(path => $self->path('embed-link-man'),
+                         COMMAND => $command,
+                         TEXT => $comment || '',
+                         MANPAGE => $manpage,
+                         SECTION => $section || ''
+                        );
+         }
+      }
+   }
+  elsif($command eq 'E')
+   { $self->expand(path => $self->path('embed-escape'),
+                   COMMAND => $command,
+                   TEXT => defined($ENTITIES{$text}) ? $ENTITIES{$text} : $text,
+                   ESCAPE => $text
+                  );
+   }
+  else
+   { $self->expand(path => $self->path('embed-unknown'),
+                   COMMAND => $command,
+                   TEXT => $text
+                  );
+   }
+}
+
+##
+# Looks through the @INC in search of the module file return the full
+# name or undef.
+#
+# Static method, does not get reference to $self!
+#
+my %module_cache;
+sub find_module_file ($$)
+{ my $self=shift;
+  my $module=shift;
+  #dprint "Looking for '$module'";
+  my $file=$INC{$module} || $module_cache{$module};
+  return $file if $file;
+  my $mp=$module;
+  $mp=~s/::/\//g;
+  $mp=~s/\s//g;
+  foreach my $dir (@INC)
+   { if(-r "$dir/${mp}.pm")
+      { $file="$dir/${mp}.pm";
+        last;
+      }
+   }
+  $file;
+}
+ 
+###############################################################################
+1;
